ae7366fc3056f7b6053199713d6dcb4072bdd9d6
Elodie Royant <eroyant@gmail.com>
2017-09-13 23:10:48 +0200

    almost with a reccursion - 7 swaps - but it lacks a stopping condition

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,29 +1,27 @@
-sequence = [4, 3, 5, 0, 1]
-swaps = 0
-
-result = sequence
-
-previous = result[0]
-current = result[1]
-i=0
-while current
-  p "##################"
-  p i
-  p result
-  previous = result[i]
+$swaps=0
+$i=0
+def bubble_sort(sequence)
+  p $i
+  p sequence
+  result = sequence
+  previous = result[$i]
   p previous
-  current = result[i+1]
+  current = result[$i+1]
   p current
-  p swaps
-  unless current.nil?
+  p $swaps
+  if current != nil
     if previous > current
-    result[i] = current
-    result[i+1] = previous
-    swaps += 1
+      result[$i] = current
+      result[$i+1] = previous
+      $swaps += 1
     end
   end
-  i < 4 || current.nil? ? i+=1 : i=0
+  $i < 4 && current != nil ? $i+=1 : $i=0
+  return bubble_sort(result)
 end
 
-puts "Final result: #{result}"
-puts "Swaps: #{swaps}"
+sequence = [4, 3, 5, 0, 1]
+
+
+puts "Final result: #{bubble_sort(sequence)}"
+puts "Swaps: #{$swaps}"

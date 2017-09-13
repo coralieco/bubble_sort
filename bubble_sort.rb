5be710449acbab35d7dacec1fd5759eaa6364139
Elodie Royant <eroyant@gmail.com>
2017-09-13 21:41:34 +0200

    le code en vrac :)

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,15 +2,18 @@ sequence = [4, 3, 5, 0, 1]
 swaps = 0
 
 pre_sequence = [4, 3, 5, 0, 1]
-previous = pre_sequence[0]
-current = pre_sequence[1]
-
+#previous = pre_sequence[0]
+#current = pre_sequence[1]
 
 while current != nil
-  if current < previous
-    pre_sequence[0] = current
-    pre_sequence[1] = previous
-    swaps += 1
+  for i in 0..4
+    previous = pre_sequence[0]
+    current = pre_sequence[1]
+    if current < previous
+      pre_sequence[i] = current
+      pre_sequence[i+1] = previous
+      swaps += 1
+    end
   end
 end
 
